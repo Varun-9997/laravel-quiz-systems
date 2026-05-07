f4a5c194c67081612b0462ec26aafea3472bf5dd
Varun-9997 <dangrevarun@gmail.com>
2026-05-07 12:38:15 +0530

    all done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,7 @@ use App\Models\Admin;
 use App\Models\Category;
 use App\Models\Quiz;
 use App\Models\Mcq;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -37,7 +38,8 @@ class AdminController extends Controller
     function dashboard(){
         $admin = Session::get('admin');
         if($admin){
-            return view('admin',["name"=>$admin['name']]);
+            $users = User::orderBy('id','desc')->paginate(4);
+            return view('admin',["name"=>$admin['name'],'users'=>$users]);
         }else{
             return redirect('admin-login');
         }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,5 +9,32 @@
 </head>
 <body>
     <x-navbar name={{$name}}></x-navbar>
+    <div class=" bg-slate-50 flex flex-col items-center min-h-screen pt-5">
+        <div class="pt-5 w-200">
+            <ul class="border border-gray-200">
+                <li class="p-2">
+                        <ul class="flex justify-between font-bold">
+                            <li class="w-40">Sr. No.</li>
+                            <li class="w-80">Name</li>
+                            <li class="w-80">Email</li>
+                        </ul>
+                    </li>
+                @foreach ($users as $key=>$user)
+                    <li class=" even:bg-gray-200 p-2">
+                        <ul class="flex justify-between">
+                            <li class="w-40">{{$key+1}}</li>
+                            <li class="w-80">{{$user->name}}</li>
+                            <li class="w-80">{{$user->email}}</li>
+                        </ul>
+                    </li>
+                @endforeach
+                
+            </ul>
+        </div>
+        <div class=" mt-5">
+            {{ $users->links() }}
+        </div>
+    </div>
+
 </body>
 </html>
